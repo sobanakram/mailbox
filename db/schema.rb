935dbde165bce494c860a21807f2ba38f36864df
Muhammad Soban Akram <soban.se@gmail.com>
2021-09-18 17:54:20 +0500

    active_storage config updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_093508) do
+ActiveRecord::Schema.define(version: 2021_09_18_125334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,14 @@ ActiveRecord::Schema.define(version: 2020_03_29_093508) do
     t.bigint "byte_size", null: false
     t.string "checksum", null: false
     t.datetime "created_at", null: false
+    t.string "service_name", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "admin_users", force: :cascade do |t|
@@ -66,6 +73,7 @@ ActiveRecord::Schema.define(version: 2020_03_29_093508) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "_type", default: 0
     t.index ["discarded_at"], name: "index_emails_on_discarded_at"
     t.index ["user_id"], name: "index_emails_on_user_id"
   end
@@ -97,5 +105,6 @@ ActiveRecord::Schema.define(version: 2020_03_29_093508) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "emails", "users"
 end
