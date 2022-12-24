e8f62f9b0d31014eb4340a046a2ca65503f1e044
Muhammad Soban Akram <soban.se@gmail.com>
2022-12-24 11:22:11 +0500

    security related gems updated and only load required on home page

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -3,7 +3,7 @@ class EmailsController < ApplicationController
   before_action :set_email, only: %i[show content]
 
   def index
-    @emails = Email.with_attached_attachments.includes(:user).order(id: :desc)
+    @emails = Email.includes(:user).order(id: :desc)
   end
 
   def show
